8bee419080effedd991befc27da66df8b9c6d36f
Gabriel <reistomm@gmail.com>
2025-07-20 19:43:39 -0300

    feat: implements backend routes and authorization with bearer token

--- src/app/(public)/signin.tsx
+++ src/app/(public)/signin.tsx
@@ -1,13 +1,42 @@
+import { zodResolver } from '@hookform/resolvers/zod';
 import { router } from 'expo-router';
 import { ArrowLeftIcon } from 'lucide-react-native';
 import React from 'react';
-import { View } from 'react-native';
+import { Controller, useForm } from 'react-hook-form';
+import { Alert, View } from 'react-native';
+import z from 'zod';
+
 import { AuthLayout } from '../../components/AuthLayout';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
+import { useAuth } from '../../hooks/useAuth';
 import { colors } from '../../styles/colors';
 
+const schema = z.object({
+  email: z.email('Informe um e-mail válido'),
+  password: z.string().min(8, 'Deve conter pelo menos 8 caracteres'),
+});
+
 export default function SignIn() {
+  const form = useForm({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const { signIn } = useAuth();
+
+  const handleSubmit = form.handleSubmit(async (formData) => {
+    try {
+      await signIn(formData);
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Credenciais inválidas!');
+    }
+  });
+
   return (
     <AuthLayout
       icon="👤"
@@ -16,20 +45,38 @@ export default function SignIn() {
     >
       <View className="justify-between flex-1">
         <View className="gap-6">
-          <Input
-            label="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
-            autoCorrect={false}
-            autoComplete="email"
+          <Controller
+            control={form.control}
+            name="email"
+            render={({ field, fieldState }) => (
+              <Input
+                label="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                autoCorrect={false}
+                autoComplete="email"
+                value={field.value}
+                onChangeText={field.onChange}
+                error={fieldState.error?.message}
+              />
+            )}
           />
         
-          <Input
-            label="Senha"
-            autoCapitalize="none"
-            autoCorrect={false}
-            autoComplete="password"
-            secureTextEntry
+          <Controller
+            control={form.control}
+            name="password"
+            render={({ field, fieldState }) => (
+              <Input
+                label="Senha"
+                autoCapitalize="none"
+                autoCorrect={false}
+                autoComplete="password"
+                secureTextEntry
+                value={field.value}
+                onChangeText={field.onChange}
+                error={fieldState.error?.message}
+              />
+            )}
           />
         </View>
 
@@ -37,7 +84,13 @@ export default function SignIn() {
           <Button onPress={router.back} size="icon" color="gray">
             <ArrowLeftIcon size={20} color={colors.black[700]} />
           </Button>
-          <Button className="flex-1">Entrar</Button>
+          <Button
+            className="flex-1"
+            onPress={handleSubmit}
+            loading={form.formState.isSubmitting}
+          >
+            Entrar
+          </Button>
         </View>
       </View>
     </AuthLayout>
