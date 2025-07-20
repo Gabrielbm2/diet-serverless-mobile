f314f382adc36958dbd159aa2dcf77560dbd8c4f
Gabriel <reistomm@gmail.com>
2025-07-20 11:47:56 -0300

    feat: integrate new components and styles with Tailwind CSS and Google Fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,73 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { 
+  HostGrotesk_400Regular,
+  HostGrotesk_500Medium,
+  HostGrotesk_600SemiBold,
+  HostGrotesk_700Bold, 
+  useFonts
+ } from '@expo-google-fonts/host-grotesk';
+import * as SplashScreen from 'expo-splash-screen';
 
 import './styles/global.css';
+import { useEffect } from 'react';
+import { HomeHeader } from './components/HomeHeader';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { DateSwitcher } from './components/DateSwitcher';
+import { DailyStats } from './components/DailyStats';
+import { MealsList } from './components/MealsList';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+
+  const [loaded, error] = useFonts({
+    HostGrotesk_400Regular,
+    HostGrotesk_500Medium,
+    HostGrotesk_600SemiBold,
+    HostGrotesk_700Bold,
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
-    <View className="bg-lime-500 flex-1 items-center justify-center">
-      <Text className="text-base">Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 bg-white">
+      <SafeAreaProvider>
+        <HomeHeader />
+        <DateSwitcher />
+        <View className="mt-2">
+          <DailyStats
+            calories={{
+              current: 500,
+              goal: 2500,
+            }}
+            proteins={{
+              current: 2000,
+              goal: 2500,
+            }}
+            carbohydrates={{
+              current: 500,
+              goal: 2500,
+            }}
+            fats={{
+              current: 500,
+              goal: 2500,
+            }}
+          />
+        </View>
+
+        <View className="h-px bg-gray-200 mt-7" />
+
+        <MealsList />
+      </SafeAreaProvider>
     </View>
   );
 }
